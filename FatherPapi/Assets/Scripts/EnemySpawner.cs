3aca3dc355d0f9d2269754ddc29ad4211e9b4021
Xparallax <mariah.grech.e21867@mcast.edu.mt>
2022-12-04 23:50:44 +0100

    Enemies
    +visual indicator
    +Enemy spawner
    +Health system for both
    +player attack

--- FatherPapi/Assets/Scripts/EnemySpawner.cs
+++ FatherPapi/Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject EnemyPrefab;
+    [SerializeField]
+    private GameObject BigEnemyPrefab;
+
+    [SerializeField]
+    private float EnemyInterval = 3.5f;
+    [SerializeField]
+    private float BigEnemyInterval = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(EnemyInterval, EnemyPrefab));
+        StartCoroutine(spawnEnemy(BigEnemyInterval, BigEnemyPrefab));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private IEnumerator spawnEnemy(float interval, GameObject enemy)
+   {
+    yield return new WaitForSeconds(interval);
+    GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f,5), Random.Range(-6f,6f),0), Quaternion.identity);
+    StartCoroutine(spawnEnemy(interval, enemy));
+   }
 }
---
+++ FatherPapi/Assets/Scripts/Health.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    private int MAX_HEALTH = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           // Damage(10);
+        }   
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+             Heal(10);
+        }
+    }
+
+    public void SetHealth(int maxHealth, int health)
+    {
+        this.MAX_HEALTH = maxHealth;
+        this.health = health;
+    }
+
+    private  IEnumerator VisualIndicator(Color color)
+    {
+        GetComponent<SpriteRenderer>().color = color;
+        yield return new WaitForSeconds(0.15f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    public void Damage(int amount)
+    {
+        if(amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        this.health -= amount;
+        StartCoroutine(VisualIndicator(Color.red));
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negatice healing");
+        }
+
+        bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+        StartCoroutine(VisualIndicator(Color.green));
+
+
+        if(wouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else 
+        {
+            this.health += amount;
+        }
+
+        this.health += amount;
+    }
+
+    private void Die()
+    {
+        Debug.Log("death");
+        Destroy(gameObject);
+    }
+}
+
