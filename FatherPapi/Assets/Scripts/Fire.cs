495135a59f57285380ce4480857bd77d9490da0b
Xparallax <mariah.grech.e21867@mcast.edu.mt>
2022-12-26 17:55:20 +0100

    added bullets (needs fixing)
    added the ui for bullets and the reload keycode
    - does not shoot

---
+++ FatherPapi/Assets/Scripts/Fire.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField]
+    private Transform barrel;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private GameObject[] ammo;
+
+    private int ammoAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      for (int i = 0; i <= 2; i++)
+      {
+        ammo[i].gameObject.SetActive(false);
+      }   
+      ammoAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q) && ammoAmount > 0)
+        {
+            var spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+            spawnedBullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 500f);
+            ammoAmount -= 1;
+            ammo[ammoAmount].gameObject.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            ammoAmount = 3;
+            for (int i = 0; i <= 2; i++)
+            {
+                ammo[i].gameObject.SetActive(true);
+            }
+        }
+    }
+}
